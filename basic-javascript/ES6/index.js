37bbb9406b6bfcb8b53d20d6b696eaddb61ea6b2
naimur <naimur202165@gmail.com>
2025-02-06 18:53:46 +0600

    working

---
+++ basic-javascript/ES6/index.js
@@ -0,0 +1,31 @@
+
+
+
+console.log("hello world")
+
+// Learning Javasxript
+
+const person={
+    name:"John",
+    age:30,
+    weight:70,
+    height:170
+}
+
+
+console.log(person.name);
+
+
+function name(a,b){
+    return a+b;
+}
+
+
+
+console.log(name(2,3));
+
+let text = "Please locate where 'locate' occurs!";
+let pos = text.indexOf("locate");
+console.log(pos);
+
+
